faafecffab15b59f93be5a724db4058c874a853b
Vidsneezes <moraleszez@gmail.com>
2017-03-06 11:47:11 -0600

    added moving logic to blocks

--- public/game.js
+++ public/game.js
@@ -18,20 +18,29 @@ var presets = {
 var style = { font: "35px Arial", fill: "#749de0", align: "center" };
 
 BoardPiece.prototype = {
-    indexedPosition: {
-        x: 0,
-        y: 0
-    },
     init: function(i, j) {
         this.baseValue = 2;
-        this.indexedPosition.x = i;
-        this.indexedPosition.y = j;
+        this.indexedPosition = {
+            x:i,y:j
+        }
         this.sprite = game.add.sprite(i*presets.tileWidth+presets.xPad,j*presets.tileHeight+presets.yPad,'playPiece');
         this.text = game.add.text(i*presets.tileWidth+presets.xPad+presets.tileWidth*0.5 , j*presets.tileHeight+presets.yPad + presets.tileHeight* 0.5, "2", style);
         this.text.anchor.set(0.5);
     },
-    movePiece: function(dir){
-        this.sprite.x += 3;
+    movePiece: function(){
+        this.sprite.x = this.indexedPosition.x*presets.tileWidth+presets.xPad;
+        this.sprite.y = this.indexedPosition.y*presets.tileHeight+presets.yPad;
+        this.text.x = this.indexedPosition.x*presets.tileWidth+presets.xPad+presets.tileWidth*0.5;
+        this.text.y = this.indexedPosition.y*presets.tileHeight+presets.yPad + presets.tileHeight* 0.5;
+    },
+    TryMove: function(x,y){
+        if(this.indexedPosition.x + x >= 0 && this.indexedPosition.y + y >= 0 &&
+            this.indexedPosition.x + x < 4 && this.indexedPosition.y + y < 4){
+            this.indexedPosition = {
+                x: this.indexedPosition.x + x, y:this.indexedPosition.y + y
+            }
+            this.movePiece();
+        }
     }
 }
 
@@ -45,7 +54,7 @@ PhaserGame.prototype = {
     },
     create: function(){
         var test = game.add.sprite(5,5,'playBoard');
-        var board = [];
+        this.board = [];
         var tiles = [1,0,0,0,
                      0,0,0,0,
                      0,0,1,0,
@@ -55,7 +64,7 @@ PhaserGame.prototype = {
               if(tiles[i+j*4] == 1){
                 var tile = new BoardPiece();
                 tile.init(i,j);
-                board.add(tile);
+                this.board.push(tile);
               }
             }
         }
@@ -68,13 +77,21 @@ PhaserGame.prototype = {
     },
     update: function(){
         if(this.upKey.downDuration(1)){
-
+           this.board.forEach(function(tile){
+               tile.TryMove(0,-1);
+           });
         }else if(this.downKey.downDuration(1)){
-
+   this.board.forEach(function(tile){
+                tile.TryMove(0,1);
+           });
         }else if(this.leftKey.downDuration(1)){
-
+   this.board.forEach(function(tile){
+                tile.TryMove(-1,0);
+           });
         }else if(this.rightKey.downDuration(1)){
-
+   this.board.forEach(function(tile){
+                tile.TryMove(1,0);
+           });
         }
     },
     resolveCollision: function(tileA,tileB,direction){
