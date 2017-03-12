87248c2663f5ac338e6e2be9535a264738edfc1c
Vidsneezes <moraleszez@gmail.com>
2017-03-12 09:37:12 -0600

    installed expose-loader and phaser via npm, added phase to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 var path = require('path');
 
+var phaserModule = path.join(__dirname,'/node_modules/phaser/');
+var phaser = path.join(phaserModule,'build/custom/phaser-split.js');
+var pixi = path.join(phaserModule,'build/custom/pixi.js');
+var p2 = path.join(phaserModule,'build/custom/p2.js');
+
 var config = {
     entry: './main.js',
     output: {
@@ -19,8 +24,30 @@ var config = {
                 query: {
                     presets: ['es2015','react']
                 }
-            }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader',
+                query: {
+                    modules: true,
+                    localIdentName: '[name]__[local]__[hash:base64:5]'
+                }
+            },
+            {test: /pixi\.js/,loader: 'expose-loader?PIXI'},
+            {test: /phaser-split\.js/,loader: 'expose-loader?Phaser'},
+            {test: /p2\.js/,loader: 'expose-loader?p2'}
         ]
+    },
+    resolve: {
+        alias: {
+            'phaser': phaser,
+            'pixi':pixi,
+            'p2': p2
+        }
     }
 }
 
